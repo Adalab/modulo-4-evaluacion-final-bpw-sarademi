398a0b0345866a792c5be5befdae7678777773dd
Sara de Miguel <saralepuz@gmail.com>
2025-08-04 13:14:09 +0200

    sección 2

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Rutas 
+app.get('/', (req, res) => {
+  res.send('¡API de Los Simpsons funcionando!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
